62e0b72d0dfb04bbb59dd50a870fa6095de9dd1c
Leandro Lopes <leandro_lopez12@yahoo.com.br>
2019-10-06 16:25:38 -0300

    install and config D3.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'LearnAngularD3';
+
+export class AppComponent implements OnInit {
+  title = 'Angular7 and Chart D3';
+
+  ngOnInit(): void {
+    console.log(d3);
+  }
 }
